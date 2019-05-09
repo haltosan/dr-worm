74a1353f6af73b9e68839ebab2184a2367a88535
haltosan <32181538+haltosan@users.noreply.github.com>
2019-05-09 01:47:38 +0000

    Adding exec
    This is the pseudo code for exec

---
+++ exec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#ssh root@[IP] 'bash -s' < [SCRIPT_NAME]
+
+#find connect to each vulnerable device
+
+#Loop:
+
+#generate a passwd
+
+ #pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+#change password in loop
+
+ #echo -e "[PASS\nPASS]" | passwd [USER]
+
+#log the password
+
+#End loop:
