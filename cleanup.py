da85dfae97cd4e2344d4ac6445f73d357fc41801
haltosan <32181538+haltosan@users.noreply.github.com>
2019-05-09 17:06:19 -0700

    Create cleanup.py
    Made a file to filter through the nmap scans to find all ips

---
+++ cleanup.py
@@ -0,0 +1,23 @@
+'''
+task: get only the ipaddresses with 22 open in the text file
+output to hostList
+'''
+
+a.open("sshOpen","r")
+read=a.read()
+a.close()
+
+rmFilter="[STRING]"
+rm1="[STRING]"
+rm2="[STRING]"
+successStr="[STRING]"
+
+cleaned=read.replace(rmFilter,"")
+cleaned1=cleaned.replace(rm1,"")
+cleaned2=cleaned1.replace(rm2,"")
+lines=cleaned2.split("\n")
+out=[]
+for i in lines:
+  if(successStr in i):
+    out.append(i)
+print("out",out)
