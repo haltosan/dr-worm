8a6a2781364c0b374474ee923f54187ac31bd9b6
haltosan <32181538+haltosan@users.noreply.github.com>
2019-05-10 02:48:36 +0000

    Added notes and clear
    Made the note section for pseudo code and filter strings

--- ipScan.sh
+++ ipScan.sh
@@ -1,4 +1,7 @@
 echo "finding curent subnet/ip range"
 lIp=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-nmap -sn {$lIp}"/24" -oN liveIps
-nmap -iL liveIps -p22 -oN sshOpen
+echo "local ip is: " $lIp
+nmap -sn $lIp"/24" -oN liveIps
+echo "*********************************************************************************"
+cat liveIps
+#nmap -iL liveIps -p22 -oN sshOpen
