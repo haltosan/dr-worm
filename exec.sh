2ce25d91b264afb907630a1c5f2b23565c747401
haltosan <32181538+haltosan@users.noreply.github.com>
2019-05-22 12:36:48 -0700

    Update exec.sh
    Added auto integration

--- exec.sh
+++ exec.sh
@@ -2,20 +2,28 @@
 
 #    ssh root@[IP] 'bash -s' < [SCRIPT_NAME]
 
+#take the passwords
 
-#generate a passwd
+ #p1=$1
+ #p2=$2
+ #p3=$3
 
- #pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 #connect to the vulnerable device
 
 #run auto
 
+  #./auto $p1 $p2 $p3
+
+#generate a passwd
+
+ #pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
 #change password
 
  #echo -e "$pass\n$pass" | passwd [USER]
 
-#log the password localy
+#exit && log the password localy
 
  #echo "$pass,[IP],[NAME]" >> log.[TIME]
 
